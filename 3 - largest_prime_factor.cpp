7e203976f4beae1eda5717e85c8f2233f5939ecd
Tolby <tolbs@users.noreply.github.com>
2018-02-25 13:54:13 -0800

    Uploaded template

---
+++ 3 - largest_prime_factor.cpp
@@ -0,0 +1,28 @@
+/*
+    @file       3 - largest_prime_factor.cpp
+    @author     Tolby Lam
+    @date       2/25/2018
+    @version    1.0
+
+    @brief      The prime factors of 13195 are 5, 7, 13 and 29.
+				What is the largest prime factor of the number 600851475143?
+
+    @TODO       -task
+*/
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+
+//==function headers===========================================================
+
+
+//==global variables===========================================================
+
+
+//==main function==============================================================
+int main() {
+    cout << "Hello, world!";
+    return 1;
+}
